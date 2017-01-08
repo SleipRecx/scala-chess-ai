7b6ac99395422807a9ba231275eb1a1a985c46c1
Markus Andresen <markus.andresen94@gmail.com>
2017-01-08 15:21:10 +0100

    added value to pieces

---
+++ src/AI/Helpers/PieceValues.scala
@@ -0,0 +1,19 @@
+package AI.Helpers
+
+object PieceValues {
+
+  def KingValue: Integer =  20000
+
+  def QueenValue: Integer = 900
+
+  def RookValue: Integer = 500
+
+  def BishopValue: Integer = 330
+
+  def KnightValue: Integer = 320
+
+  def PawnValue: Integer = 100
+
+  def MobilityValue: Integer = 10
+
+}
