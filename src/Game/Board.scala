a69db52920f7518f9c17509953d0d0ff058b8e0e
Markus Andresen <markus.andresen94@gmail.com>
2017-01-11 20:53:41 +0100

    testBoardMethod implemented

--- src/Game/Board.scala
+++ src/Game/Board.scala
@@ -13,73 +13,6 @@ class Board{
   var turn: Color = Color.White
   var state: Array[Array[Spot]] = setupChessBoard
 
-  private def setupChessBoard: Array[Array[Spot]] = {
-    state = Array.ofDim(RowCount, ColCount)
-
-    for(i <- 0 until RowCount){
-      for(j <- 0 until ColCount){
-        state(i)(j) = new Spot(i,j)
-      }
-    }
-    state(0)(0).addPiece(new Rook(Color.White))
-    state(0)(1).addPiece(new Knight(Color.White))
-    state(0)(2).addPiece(new Bishop(Color.White))
-    state(0)(3).addPiece(new Queen(Color.White))
-    state(0)(4).addPiece(new King(Color.White))
-    state(0)(5).addPiece(new Bishop(Color.White))
-    state(0)(6).addPiece(new Knight(Color.White))
-    state(0)(7).addPiece(new Rook(Color.White))
-
-    for(i <- 0 until ColCount){
-      state(1)(i).addPiece(new Pawn(Color.White))
-    }
-
-    state(7)(0).addPiece(new Rook(Color.Black))
-    state(7)(1).addPiece(new Knight(Color.Black))
-    state(7)(2).addPiece(new Bishop(Color.Black))
-    state(7)(3).addPiece(new Queen(Color.Black))
-    state(7)(4).addPiece(new King(Color.Black))
-    state(7)(5).addPiece(new Bishop(Color.Black))
-    state(7)(6).addPiece(new Knight(Color.Black))
-    state(7)(7).addPiece(new Rook(Color.Black))
-
-    for(i <- 0 until ColCount){
-      state(6)(i).addPiece(new Pawn(Color.Black))
-    }
-
-    state
-  }
-
-  private def switchTurn(): Unit = {
-     turn = if(turn == Color.White) Color.Black else Color.White
-  }
-
-  private def isBoardInCheck(state: Array[Array[Spot]], color: Color): Boolean = {
-    var king: (Integer,Integer) = (0,0)
-    var inCheck = false
-
-    for(i <- state.indices){
-      for(j <- state.indices){
-        if(state(i)(j).piece != null) {
-          if (state(i)(j).piece.pieceType == Type.King && color == state(i)(j).piece.color) {
-            king = (i, j)
-          }
-        }
-      }
-    }
-
-    for(i <- state.indices) {
-      for (j <- state.indices) {
-        if (state(i)(j).isOccupied && state(i)(j).piece.color != color) {
-          if (state(i)(j).piece.isValidMoveSet(state, new Move((i,j),(king._1, king._2)))) {
-            inCheck = true
-          }
-        }
-      }
-    }
-    inCheck
-  }
-
   def getAllOccupiedSpotsByColor(c: Color): ArrayBuffer[Spot] = {
     var spots = ArrayBuffer[Spot]()
     state.foreach(e1 => e1.foreach(e2 => if(e2.isOccupied) if(e2.piece.color == c) spots += e2))
@@ -98,7 +31,7 @@ class Board{
       for(j <- 0 until ColCount){
         copyState(i)(j) = new Spot(i,j)
         if(state(i)(j).isOccupied){
-            state(i)(j).piece.pieceType match {
+          state(i)(j).piece.pieceType match {
             case Type.Pawn => copyState(i)(j).piece = new Pawn(state(i)(j).piece.color)
             case Type.Rook => copyState(i)(j).piece = new Rook(state(i)(j).piece.color)
             case Type.Bishop => copyState(i)(j).piece = new Bishop(state(i)(j).piece.color)
@@ -190,6 +123,89 @@ class Board{
     spot.removePiece()
 
     switchTurn()
+  }
+
+  private def setupChessBoard: Array[Array[Spot]] = {
+    state = Array.ofDim(RowCount, ColCount)
+
+    for(i <- 0 until RowCount){
+      for(j <- 0 until ColCount){
+        state(i)(j) = new Spot(i,j)
+      }
+    }
+    state(0)(0).addPiece(new Rook(Color.White))
+    state(0)(1).addPiece(new Knight(Color.White))
+    state(0)(2).addPiece(new Bishop(Color.White))
+    state(0)(3).addPiece(new Queen(Color.White))
+    state(0)(4).addPiece(new King(Color.White))
+    state(0)(5).addPiece(new Bishop(Color.White))
+    state(0)(6).addPiece(new Knight(Color.White))
+    state(0)(7).addPiece(new Rook(Color.White))
+
+    for(i <- 0 until ColCount){
+      state(1)(i).addPiece(new Pawn(Color.White))
+    }
+
+    state(7)(0).addPiece(new Rook(Color.Black))
+    state(7)(1).addPiece(new Knight(Color.Black))
+    state(7)(2).addPiece(new Bishop(Color.Black))
+    state(7)(3).addPiece(new Queen(Color.Black))
+    state(7)(4).addPiece(new King(Color.Black))
+    state(7)(5).addPiece(new Bishop(Color.Black))
+    state(7)(6).addPiece(new Knight(Color.Black))
+    state(7)(7).addPiece(new Rook(Color.Black))
+
+    for(i <- 0 until ColCount){
+      state(6)(i).addPiece(new Pawn(Color.Black))
+    }
+
+    state
+  }
+
+  private def setupTestChessBoard:  Array[Array[Spot]] = {
+    state = Array.ofDim(RowCount, ColCount)
+
+    for(i <- 0 until RowCount){
+      for(j <- 0 until ColCount){
+        state(i)(j) = new Spot(i,j)
+      }
+    }
+
+    state(0)(0).addPiece(new Knight(Color.White))
+    state(6)(0).addPiece(new Pawn(Color.White))
+    state(6)(0).piece.moved = true
+    state(7)(7).addPiece(new Pawn(Color.Black))
+    state
+  }
+
+  private def switchTurn(): Unit = {
+     turn = if(turn == Color.White) Color.Black else Color.White
+  }
+
+  private def isBoardInCheck(state: Array[Array[Spot]], color: Color): Boolean = {
+    var king: (Integer,Integer) = (0,0)
+    var inCheck = false
+
+    for(i <- state.indices){
+      for(j <- state.indices){
+        if(state(i)(j).piece != null) {
+          if (state(i)(j).piece.pieceType == Type.King && color == state(i)(j).piece.color) {
+            king = (i, j)
+          }
+        }
+      }
+    }
+
+    for(i <- state.indices) {
+      for (j <- state.indices) {
+        if (state(i)(j).isOccupied && state(i)(j).piece.color != color) {
+          if (state(i)(j).piece.isValidMoveSet(state, new Move((i,j),(king._1, king._2)))) {
+            inCheck = true
+          }
+        }
+      }
+    }
+    inCheck
   }
 
 }
